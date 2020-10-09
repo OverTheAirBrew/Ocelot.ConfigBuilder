dbd8d60e35f38de01afeeec18d191046a943a5f4
sharpn <nick.sharp.87@gmail.com>
2020-10-09 09:41:12 +0100

    removing unneeded dependencuy

--- src/Commands/GenerateOcelotConfigCommand.cs
+++ src/Commands/GenerateOcelotConfigCommand.cs
@@ -1,4 +1,3 @@
-using System.Threading.Tasks.Dataflow;
 using System.IO;
 using System;
 using System.Linq;
@@ -20,141 +19,141 @@ using Microsoft.AspNetCore.Routing;
 namespace TheNerdyBrewingCo.Api.Commands
 {
 
-  [Description("This will generate the ocelot config from the current api")]
-  public class GenerateOcelotConfigCommand : OaktonCommand<NetCoreInput>
-  {
-    private readonly IDictionary<string, Type> _constraintMap;
-
-    public GenerateOcelotConfigCommand()
+    [Description("This will generate the ocelot config from the current api")]
+    public class GenerateOcelotConfigCommand : OaktonCommand<NetCoreInput>
     {
-      var routeOptions = new RouteOptions();
-      _constraintMap = routeOptions.ConstraintMap;
-    }
+        private readonly IDictionary<string, Type> _constraintMap;
 
-    public override bool Execute(NetCoreInput input)
-    {
-      var host = input.BuildHost();
-      var serviceType = typeof(IApiDescriptionGroupCollectionProvider);
-
-      var ocelotBaseConfig = host.Services.GetService(typeof(IOcelotConfigBuilderConfiguration)) as IOcelotConfigBuilderConfiguration;
-
-      if (!(host.Services.GetService(serviceType) is IApiDescriptionGroupCollectionProvider explorer))
-        return true;
-
-      var results = explorer
-        .ApiDescriptionGroups
-        .Items
-        .SelectMany(x => x.Items);
-
-      var grouped = results.GroupBy(r => r.RelativePath);
-
-      var config = new OcelotConfig()
-      {
-      };
-
-      foreach (var group in grouped)
-      {
-        var ocelotRoutes = group.Select(x => x.ActionDescriptor.EndpointMetadata.Where(y => y.GetType() == typeof(OcelotRoute))).FirstOrDefault();
-
-        var ocelotRoute = GetRoute(ocelotRoutes, group.Key);
-
-        if (ocelotRoute == null) continue;
-
-        var claimsToHeaders = group.Select(x => x.ActionDescriptor.EndpointMetadata.Where(y => y.GetType() == typeof(OcelotClaimToHeaderAttribute))).FirstOrDefault();
-
-        var downStreamRoute = group.Key;
-        var upstreamMethods = group.Select(x => x.HttpMethod).ToArray();
-
-        // var upstreamPath = upstreamUrl != null ? formatUrl(upstreamUrl.Url, ocelotBaseConfig.PrefixToRemove) : formatUrl(downStreamRoute, ocelotBaseConfig.PrefixToRemove);
-
-        var route = new OcelotConfigRoute
+        public GenerateOcelotConfigCommand()
         {
-          DownstreamPathTemplate = formatUrl(downStreamRoute),
-          DownstreamHostAndPorts = new List<OcelotConfigBuilderDownstreamHost> { ocelotBaseConfig.DownstreamHost },
-          UpstreamPathTemplate = formatUrl(ocelotRoute.OcelotTemplate),
-          UpstreamHttpMethod = upstreamMethods,
-        };
-
-        foreach (var claim in claimsToHeaders)
-        {
-          var castClaim = claim as OcelotClaimToHeaderAttribute;
-
-          route.AddHeadersToRequest.Add(castClaim.HeaderName, $"Claims[{castClaim.Claim}]>{castClaim.ValuePath}".Trim());
+            var routeOptions = new RouteOptions();
+            _constraintMap = routeOptions.ConstraintMap;
         }
 
-        if (ocelotRoute.AuthenticationProvider != null)
+        public override bool Execute(NetCoreInput input)
         {
-          route.AuthenticationOptions = new OcelotConfigRouteAuthOptions
-          {
-            AuthenticationProviderKey = ocelotRoute.AuthenticationProvider
-          };
+            var host = input.BuildHost();
+            var serviceType = typeof(IApiDescriptionGroupCollectionProvider);
+
+            var ocelotBaseConfig = host.Services.GetService(typeof(IOcelotConfigBuilderConfiguration)) as IOcelotConfigBuilderConfiguration;
+
+            if (!(host.Services.GetService(serviceType) is IApiDescriptionGroupCollectionProvider explorer))
+                return true;
+
+            var results = explorer
+              .ApiDescriptionGroups
+              .Items
+              .SelectMany(x => x.Items);
+
+            var grouped = results.GroupBy(r => r.RelativePath);
+
+            var config = new OcelotConfig()
+            {
+            };
+
+            foreach (var group in grouped)
+            {
+                var ocelotRoutes = group.Select(x => x.ActionDescriptor.EndpointMetadata.Where(y => y.GetType() == typeof(OcelotRoute))).FirstOrDefault();
+
+                var ocelotRoute = GetRoute(ocelotRoutes, group.Key);
+
+                if (ocelotRoute == null) continue;
+
+                var claimsToHeaders = group.Select(x => x.ActionDescriptor.EndpointMetadata.Where(y => y.GetType() == typeof(OcelotClaimToHeaderAttribute))).FirstOrDefault();
+
+                var downStreamRoute = group.Key;
+                var upstreamMethods = group.Select(x => x.HttpMethod).ToArray();
+
+                // var upstreamPath = upstreamUrl != null ? formatUrl(upstreamUrl.Url, ocelotBaseConfig.PrefixToRemove) : formatUrl(downStreamRoute, ocelotBaseConfig.PrefixToRemove);
+
+                var route = new OcelotConfigRoute
+                {
+                    DownstreamPathTemplate = formatUrl(downStreamRoute),
+                    DownstreamHostAndPorts = new List<OcelotConfigBuilderDownstreamHost> { ocelotBaseConfig.DownstreamHost },
+                    UpstreamPathTemplate = formatUrl(ocelotRoute.OcelotTemplate),
+                    UpstreamHttpMethod = upstreamMethods,
+                };
+
+                foreach (var claim in claimsToHeaders)
+                {
+                    var castClaim = claim as OcelotClaimToHeaderAttribute;
+
+                    route.AddHeadersToRequest.Add(castClaim.HeaderName, $"Claims[{castClaim.Claim}]>{castClaim.ValuePath}".Trim());
+                }
+
+                if (ocelotRoute.AuthenticationProvider != null)
+                {
+                    route.AuthenticationOptions = new OcelotConfigRouteAuthOptions
+                    {
+                        AuthenticationProviderKey = ocelotRoute.AuthenticationProvider
+                    };
+                }
+
+                config.Routes.Add(route);
+            }
+
+            config.GlobalConfiguration.BaseUrl = ocelotBaseConfig.BaseUrl;
+
+            var data = JsonConvert.SerializeObject(config, new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore,
+                Formatting = Formatting.Indented
+            });
+
+            Console.WriteLine(data);
+
+            var serializer = new SerializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
+            var configmap = new Configmap
+            {
+                Metadata = new ConfigmapMetadata
+                {
+                    Name = ocelotBaseConfig.Kubernetes.Name,
+                    Namespace = ocelotBaseConfig.Kubernetes.Namespace
+                },
+            };
+
+            configmap.Data.Add("ocelot.json", data);
+
+            var output = serializer.Serialize(configmap);
+
+            File.WriteAllText(ocelotBaseConfig.OutputFileName, output);
+
+            return true;
         }
 
-        config.Routes.Add(route);
-      }
-
-      config.GlobalConfiguration.BaseUrl = ocelotBaseConfig.BaseUrl;
-
-      var data = JsonConvert.SerializeObject(config, new JsonSerializerSettings
-      {
-        NullValueHandling = NullValueHandling.Ignore,
-        Formatting = Formatting.Indented
-      });
-
-      Console.WriteLine(data);
-
-      var serializer = new SerializerBuilder().WithNamingConvention(CamelCaseNamingConvention.Instance).Build();
-      var configmap = new Configmap
-      {
-        Metadata = new ConfigmapMetadata
+        private string formatUrl(string url)
         {
-          Name = ocelotBaseConfig.Kubernetes.Name,
-          Namespace = ocelotBaseConfig.Kubernetes.Namespace
-        },
-      };
-
-      configmap.Data.Add("ocelot.json", data);
-
-      var output = serializer.Serialize(configmap);
-
-      File.WriteAllText(ocelotBaseConfig.OutputFileName, output);
-
-      return true;
-    }
-
-    private string formatUrl(string url)
-    {
-      if (url.Substring(0, 1) == "/") return url;
-      return $"/{url}";
-    }
-
-    public OcelotRoute GetRoute(IEnumerable<object> ocelotRoutes, string routeTemplate)
-    {
-      foreach (var route in ocelotRoutes.OrderByDescending(x => (x as OcelotRoute).OcelotTemplate.Length))
-      {
-        var template = (route as OcelotRoute).Template;
-        var formattedTemplate = formatRouteTemplate(template);
-
-        var contains = routeTemplate.Contains(formattedTemplate);
-        if (contains)
-        {
-          return (route as OcelotRoute);
+            if (url.Substring(0, 1) == "/") return url;
+            return $"/{url}";
         }
-      }
 
-      var defaultRoute = ocelotRoutes.First();
-      return (defaultRoute as OcelotRoute);
+        public OcelotRoute GetRoute(IEnumerable<object> ocelotRoutes, string routeTemplate)
+        {
+            foreach (var route in ocelotRoutes.OrderByDescending(x => (x as OcelotRoute).OcelotTemplate.Length))
+            {
+                var template = (route as OcelotRoute).Template;
+                var formattedTemplate = formatRouteTemplate(template);
+
+                var contains = routeTemplate.Contains(formattedTemplate);
+                if (contains)
+                {
+                    return (route as OcelotRoute);
+                }
+            }
+
+            var defaultRoute = ocelotRoutes.First();
+            return (defaultRoute as OcelotRoute);
+        }
+
+        private string formatRouteTemplate(string routeTemplate)
+        {
+            foreach (var constraint in _constraintMap)
+            {
+                var key = $":{constraint.Key}";
+                routeTemplate = routeTemplate.Replace(key, "");
+            }
+
+            return routeTemplate;
+        }
     }
-
-    private string formatRouteTemplate(string routeTemplate)
-    {
-      foreach (var constraint in _constraintMap)
-      {
-        var key = $":{constraint.Key}";
-        routeTemplate = routeTemplate.Replace(key, "");
-      }
-
-      return routeTemplate;
-    }
-  }
 }
